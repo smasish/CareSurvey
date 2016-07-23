6ac0707f6d633b7a376f5fa85538dfeb95d9e89d
Shantanu Saha <shantanucse18@gmail.com>
2016-07-24 01:40:35 +0600

    Filter user by supervisor login

--- app/src/main/java/caresurvey/sci/com/caresurvey/activity/FacilityAdminActivity.java
+++ app/src/main/java/caresurvey/sci/com/caresurvey/activity/FacilityAdminActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import caresurvey.sci.com.caresurvey.R;
+import caresurvey.sci.com.caresurvey.utils.AppUtils;
 
 /**
  * Created by shantanu on 6/24/16.
@@ -20,6 +23,7 @@ public class FacilityAdminActivity extends AppCompatActivity implements View.OnC
 
     private Spinner userSpinner;
     private Spinner facilitySpinner;
+    private ArrayAdapter<String> mAdapter;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,40 @@ public class FacilityAdminActivity extends AppCompatActivity implements View.OnC
         findViewById(R.id.next).setOnClickListener(this);
         userSpinner = (Spinner) findViewById(R.id.user_spinner);
         facilitySpinner = (Spinner) findViewById(R.id.facility_spinner);
+        mAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item);
+        mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        String username = AppUtils.getUserName(this);
+        mAdapter.add("Select user");
+        if("supervisor_hb".equals(username)){
+            mAdapter.add("user_hb1");
+            mAdapter.add("user_hb2");
+            mAdapter.add("user_hb3");
+        }
+        else if("supervisor_jk".equals(username)){
+            mAdapter.add("user_jk1");
+            mAdapter.add("user_jk2");
+            mAdapter.add("user_jk3");
+        }
+        else if("supervisor_lp".equals(username)){
+            mAdapter.add("user_lp1");
+            mAdapter.add("user_lp2");
+            mAdapter.add("user_lp3");
+        }
+        else if("admin".equals(username)){
+            mAdapter.add("user_hb1");
+            mAdapter.add("user_hb2");
+            mAdapter.add("user_hb3");
+
+            mAdapter.add("user_jk1");
+            mAdapter.add("user_jk2");
+            mAdapter.add("user_jk3");
+
+            mAdapter.add("user_lp1");
+            mAdapter.add("user_lp2");
+            mAdapter.add("user_lp3");
+        }
+        userSpinner.setAdapter(mAdapter);
     }
 
     @Override
